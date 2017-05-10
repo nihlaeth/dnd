fc9a73b5a197b0c339829b13028d03e92cf8703f
nihlaeth <info@nihlaeth.nl>
2017-05-10 20:34:53 +0200

    refactor index page generation with the help of @sietse

--- dnd/themes/default/index.py
+++ dnd/themes/default/index.py
@@ -4,9 +4,32 @@ from dnd.themes.default.base import base
 from dnd.html.bootstrap import (
     Style, a_button, b_button, badge, collapse, async_form, b_table)
 
+# pylint: disable=invalid-name
+def _character_table_row(name, hp, _id, created_at, **_):
+    """
+    Render a row of the character table.
+
+    The `**unused` lets you call `character_table_row(**character)`.
+    Accessing the variables as function arguments instead of via
+    `character['id_']` reduces visual noise
+    """
+    alive_badge = badge("R.I.P.") if hp < -9 else ''
+    return {
+        "Name": [a_button(name, url=f"/{_id}/{name}/"), alive_badge],
+        "Date created": [created_at.date()],
+    }
+
+def _character_table(characters):
+    table = b_table(
+        header=["Name", "Date created"],
+        body=[_character_table_row(**character) for character in characters],
+    )
+    table.children[0].attributes['id_'] = "character-table"
+    return table
+
 def index(characters):
     """Index page."""
-    content = []
+    # Create a form and a button that collapses it
     add_character_button = b_button("Add", style=Style.INFO)
     add_character_form = div(id_="add-character-form")(
         async_form(
@@ -21,24 +44,11 @@ def index(characters):
                 'placeholder': "Character name"}],
             inline=True))
     collapse(add_character_form, add_character_button)
-    content.append(h1("Characters", add_character_button))
-    content.append(add_character_form)
-    content.append(br())
 
-    table_body = []
-    for character in characters:
-        row = {
-            "Name": [a_button(
-                character['name'],
-                url=f"/{character['_id']}/{character['name']}/")],
-            "Date created": [character['created_at'].date()]}
-        if character['hp'] < -9:
-            row['Name'].append(badge("R.I.P."))
-        table_body.append(row)
-    character_table = b_table(
-        header=["Name", "Date created"],
-        body=table_body)
-    character_table.children[0].attributes['id_'] = "character-table"
-    content.append(character_table)
-
+    content = [
+        h1("Characters", add_character_button),
+        add_character_form,
+        br(),
+        _character_table(characters),
+    ]
     return base(characters, "Home", content=content)
