186c0a926f90675ab678eb6546f0eac65371f88b
nihlaeth <info@nihlaeth.nl>
2017-05-10 20:47:57 +0200

    refactor character page generation

--- dnd/themes/default/character/character.py
+++ dnd/themes/default/character/character.py
@@ -7,36 +7,32 @@ from dnd.html.bootstrap import (
 
 def character_(characters, editing_privileges, character):
     """Character page."""
-    body_title = [span(id_="name-value")(character['name'])]
-    if character['hp'] < -9:
-        body_title.append(span(id_="alive")(badge("R.I.P.")))
-    else:
-        body_title.append(span(id_="alive"))
-    content = []
-    if editing_privileges:
-        edit_name_button = b_button("Edit", style=Style.INFO)
-        edit_name_form = div(id_="edit-name-form")(
-            async_form(
-                form_name="edit-name",
-                action=f"/api/{character['_id']}/name/",
-                submit_button="Edit",
-                inputs=[{
-                    'label': ['Name:'],
-                    'type': "input",
-                    'id': "character-name",
-                    'name': "name",
-                    'value': character['name']}],
-                inline=True))
-        collapse(edit_name_form, edit_name_button)
-        body_title.append(edit_name_button)
-        content.append(edit_name_form)
-    content.append(br())
-    content.append(div(id_="edit-accordion")(grid(
-        {'width': 4, 'content': [actions(character)]},
-        {'width': 4, 'content': []},
-        {'width': 4, 'content': []})))
+    edit_name_button = b_button(
+        "Edit", style=Style.INFO) if editing_privileges else ''
+    edit_name_form = div(id_="edit-name-form")(
+        async_form(
+            form_name="edit-name",
+            action=f"/api/{character['_id']}/name/",
+            submit_button="Edit",
+            inputs=[{
+                'label': ['Name:'],
+                'type': "input",
+                'id': "character-name",
+                'name': "name",
+                'value': character['name']}],
+            inline=True)) if editing_privileges else ''
+    collapse(edit_name_form, edit_name_button)
     return base(
         characters,
         character['name'],
-        body_title=body_title,
-        content=content)
+        body_title=[
+            span(id_="name-value")(character['name']),
+            span(id_="alive")(badge("R.I.P.") if character['hp'] < -9 else ''),
+            edit_name_button],
+        content=[
+            edit_name_form,
+            br(),
+            div(id_="edit-accordion")(grid(
+                {'width': 4, 'content': [actions(character)]},
+                {'width': 4, 'content': []},
+                {'width': 4, 'content': []}))])
